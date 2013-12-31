0fc40f035bf1cffabad95333de1840c1010b8c52
aaronykng <aaronykng@gmail.com>
2013-12-30 16:41:27 -0800

    Moved border creation to separate method.

--- UIView+Borders.m
+++ UIView+Borders.m
@@ -15,20 +15,11 @@
 //////////
 
 -(void)addTopBorderWithHeight: (CGFloat)height andColor:(UIColor*)color{
-    
-    CALayer *border = [CALayer layer];
-    border.frame = CGRectMake(0, 0, self.frame.size.width, height);
-    [border setBackgroundColor:color.CGColor];
-    [self.layer addSublayer:border];
-    
+    [self addOneSidedBorderWithFrame:CGRectMake(0, 0, self.frame.size.width, height) andColor:color];
 }
 
 -(void)addViewBackedTopBorderWithHeight: (CGFloat)height andColor:(UIColor*)color{
-    
-    UIView *border = [[UIView alloc]initWithFrame:CGRectMake(0, 0, self.frame.size.width, height)];
-    [border setBackgroundColor:color];
-    [self addSubview:border];
-    
+    [self addViewBackedOneSidedBorderWithFrame:CGRectMake(0, 0, self.frame.size.width, height) andColor:color];
 }
 
 //////////
@@ -36,16 +27,11 @@
 //////////
 
 -(void)addRightBorderWithWidth: (CGFloat)width andColor:(UIColor*)color{
-    CALayer *border = [CALayer layer];
-    border.frame = CGRectMake(self.frame.size.width-width, 0, width, self.frame.size.height);
-    [border setBackgroundColor:color.CGColor];
-    [self.layer addSublayer:border];
+    [self addOneSidedBorderWithFrame:CGRectMake(self.frame.size.width-width, 0, width, self.frame.size.height) andColor:color];
 }
 
 -(void)addViewBackedRightBorderWithWidth: (CGFloat)width andColor:(UIColor*)color{
-    UIView *border = [[UIView alloc]initWithFrame:CGRectMake(self.frame.size.width-width, 0, width, self.frame.size.height)];
-    [border setBackgroundColor:color];
-    [self addSubview:border];
+    [self addViewBackedOneSidedBorderWithFrame:CGRectMake(self.frame.size.width-width, 0, width, self.frame.size.height) andColor:color];
 }
 
 //////////
@@ -53,16 +39,11 @@
 //////////
 
 -(void)addBottomBorderWithHeight: (CGFloat)height andColor:(UIColor*)color{
-    CALayer *border = [CALayer layer];
-    border.frame = CGRectMake(0, self.frame.size.height-height, self.frame.size.width, height);
-    [border setBackgroundColor:color.CGColor];
-    [self.layer addSublayer:border];
+    [self addOneSidedBorderWithFrame:CGRectMake(0, self.frame.size.height-height, self.frame.size.width, height) andColor:color];
 }
 
 -(void)addViewBackedBottomBorderWithHeight: (CGFloat)height andColor:(UIColor*)color{
-    UIView *border = [[UIView alloc]initWithFrame:CGRectMake(0, self.frame.size.height-height, self.frame.size.width, height)];
-    [border setBackgroundColor:color];
-    [self addSubview:border];
+    [self addViewBackedOneSidedBorderWithFrame:CGRectMake(0, self.frame.size.height-height, self.frame.size.width, height) andColor:color];
 }
 
 //////////
@@ -70,14 +51,28 @@
 //////////
 
 -(void)addLeftBorderWithWidth: (CGFloat)width andColor:(UIColor*)color{
+    [self addOneSidedBorderWithFrame:CGRectMake(0, 0, width, self.frame.size.height) andColor:color];
+}
+
+-(void)addViewBackedLeftBorderWithWidth: (CGFloat)width andColor:(UIColor*)color{
+    [self addViewBackedOneSidedBorderWithFrame:CGRectMake(0, 0, width, self.frame.size.height) andColor:color];
+}
+
+//////////
+// Private: Our methods call these to add their borders.
+//////////
+
+-(void)addOneSidedBorderWithFrame:(CGRect)frame andColor:(UIColor*)color
+{
     CALayer *border = [CALayer layer];
-    border.frame = CGRectMake(0, 0, width, self.frame.size.height);
+    border.frame = frame;
     [border setBackgroundColor:color.CGColor];
     [self.layer addSublayer:border];
 }
 
--(void)addViewBackedLeftBorderWithWidth: (CGFloat)width andColor:(UIColor*)color{
-    UIView *border = [[UIView alloc]initWithFrame:CGRectMake(0, 0, width, self.frame.size.height)];
+-(void)addViewBackedOneSidedBorderWithFrame:(CGRect)frame andColor:(UIColor*)color
+{
+    UIView *border = [[UIView alloc]initWithFrame:frame];
     [border setBackgroundColor:color];
     [self addSubview:border];
 }
